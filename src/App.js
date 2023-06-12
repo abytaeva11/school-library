59b1634a7fa7884ce5945c8fe020f341918febaa
Nazidev01 <toktonalykyzynazi3675478@gmail.com>
2023-06-12 11:22:59 +0600

    course

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import Global from "./components/Global";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/header/Header";
 import {Route, Routes} from "react-router-dom";
-import Login from "./components/Login/login";
+import Course from "./Pages/Course/course";
+import Course2 from "./Pages/Course/course2";
 
 function App() {
-  return (
-    <div className="App">
-<Header/>
-        {/*<Routes>*/}
-        {/*    <Route path={'/login'} element={<Login/>}/>*/}
-        {/*</Routes>*/}
-      <Global/>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Global/>}/>
+                <Route path='/we' element={<Course/>}/>
+
+                <Route path="/photo" element={<Course2/>}/>
+            </Routes>
+
+
+
+            <Footer/>
+        </div>
+    );
 }
+
 export default App;
