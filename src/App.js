4740db8016351a7ee3981c91fc0427c724e190a7
asadulladev <119585559+asadulladev@users.noreply.github.com>
2023-06-13 13:01:05 +0600

    fill (#11)

--- src/App.js
+++ src/App.js
@@ -1,49 +1,38 @@
 import './App.css';
-
 import Global from "./components/Global";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/header/Header";
-
 import Login from "./components/Login/login";
 import Subscribing from "./Pages/Questions/Subscribing/index";
 import { Route, Routes, useLocation } from "react-router-dom";
+import Many from "./Pages/Tabs/Many/Many";
+import Paket from "./Pages/Paket/paket";
+import React from "react";
 
 function App() {
     const location = useLocation();
-  return (
-    <div className="App">
-<Header/>
-        {/*<Routes>*/}
-        {/*    <Route path={'/login'} element={<Login/>}/>*/}
-        {/*</Routes>*/}
-        <Routes>
-            <Route path={"/subscribe"} element={<Subscribing />} />
-        </Routes>
-        {location.pathname !== "/subscribe" && (
-            <>
-                <Global />
-            </>
-        )}
-        <Footer />
-    </div>
-  );
-
-import {Route, Routes} from "react-router-dom";
-import Many from "./Pages/Tabs/Many/Many";
-
-function App() {
     return (
         <div className="App">
             <Header/>
+            {/*<Routes>*/}
+            {/*    <Route path={'/login'} element={<Login/>}/>*/}
+            {/*</Routes>*/}
             <Routes>
-                <Route path={"/"} element={<Global/>}/>
+                <Route path={"/subscribe"} element={<Subscribing/>}/>
+
                 <Route path={"/many"} element={<Many/>}/>
+                <Route path={"/many"} element={ <Paket/> }/>
             </Routes>
+            {location.pathname !== "/subscribe" && (
+                <>
+                    <Routes>
+                        <Route path={"/"} element={<Global/>}/>
+                    </Routes>
+                </>
+            )}
             <Footer/>
-
         </div>
-    )
-
+    );
 }
 
 export default App;
