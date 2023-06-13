4af97ec3a67dcc30cc581f8d9c7d45d104b3988b
abytaeva11 <113246152+abytaeva11@users.noreply.github.com>
2023-06-13 12:40:30 +0500

    nazi

--- src/Pages/Paket/paket.js
+++ src/Pages/Paket/paket.js
@@ -1,16 +1,20 @@
 import React from "react"
-import book from "./img/books.png"
+import book from "./img/tea.png"
 import "./paket.css"
+import {Link} from "react-router-dom";
+
 const Paket = () => {
-    return(
+    return (
         <div>
             <section id="paket">
                 <div className="container">
                     <div className="paket">
                         <div className="paket--text">
-                            <h2 style={{color:"white"}}>Наши курсы</h2>
+                            <h2 style={{color: "white"}}>Наши курсы</h2>
                             <p>Получайте новые знания, навыки и <br/>возможности для эффективной жизни..</p>
-                            <button className="paket--text__btn">Подобрать пакет</button>
+                            <Link to={"/buttons"}>
+                                <button className="paket--text__btn">Подобрать пакет</button>
+                            </Link>
                             <button className="paket--text__btn2">Войти</button>
                         </div>
                         <img className="paket--img" src={book} alt=""/>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -28,22 +28,16 @@ const Header = () => {
                            <Link to={'/tellegram'} className={'link-t px-4 text-amber-50'}>Наши курсы</Link>
                            <Link to={'/'} className={'link-t px-4 text-amber-50'}>О нас</Link>
                 </div>
-                    <div className="header-title flex items-center  ">
-                        <Link to="/" className={'link-t px-4'}>Главная</Link>
-                        <Link to="/we" className={'link-t px-4'}>Наши курсы</Link>
-                        <Link to={'/'} className={'link-t px-4'}>О нас</Link>
-                    </div>
 
                     <button onClick={() => navigate('/login')} className={'rounded-tr-md rounded-br-md rounded-bl-md w-32 h-10 rounded-sm flex items-center bg-white text-blue-500 flex justify-center items-center '}><CiUser className="header-icon"/>Войти</button>
-                    <div className="Menu">
-                        <button onClick={() => setBurger(!burger)} className="menu-icon">{burger? <AiOutlineCloseCircle/> : <AiOutlineMenu/>}</button>
-//                         <div className="burger" style={{display: burger? "block" : "none" }}>
-//                             <Link to={'/'} className={'link-t px-4 text-amber-50'}>Главная</Link>
-//                             <Link to={'/tellegram'} className={'link-t px-4 text-amber-50'}>Наши курсы</Link>
-//                             <Link to={'/'} className={'link-t px-4 text-amber-50'}>О нас</Link>
-//                         </div>
-
-//                     </div>
+                    {/*<div className="Menu">*/}
+                    {/*    <button onClick={() => setBurger(!burger)} className="menu-icon">{burger? <AiOutlineCloseCircle/> : <AiOutlineMenu/>}</button>*/}
+                    {/*   <div className="burger" style={{display: burger? "block" : "none" }}>*/}
+                    {/*        <Link to={'/'} className={'link-t px-4 text-amber-50'}>Главная</Link>*/}
+                    {/*         <Link to={'/tellegram'} className={'link-t px-4 text-amber-50'}>Наши курсы</Link>*/}
+                    {/*         <Link to={'/'} className={'link-t px-4 text-amber-50'}>О нас</Link>*/}
+                    {/*   </div>*/}
+                    {/*</div>*/}
                 </div>
 
 
