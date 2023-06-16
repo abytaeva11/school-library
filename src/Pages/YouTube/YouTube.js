63d033bdbbde542de7f815325dd3e58910cc3566
Nazidev01 <toktonalykyzynazi3675478@gmail.com>
2023-06-16 14:00:17 +0600

    you tube

--- src/Pages/YouTube/YouTube.js
+++ src/Pages/YouTube/YouTube.js
@@ -1,13 +1,33 @@
 import React from 'react';
-
+import "./youtube.css"
 const YouTube = () => {
     return (
         <div>
+
+            <div id="effect">
+                <div className="container">
+                    <a className="link" href="">Наши курсы / Курсы по созданию команды</a>
+                    <div className="effect">
+                        <div className="effect-title">
+                            <h1>Создание команды</h1>
+                            <p>Как правильно строить команду и эффективно ею <br/>управлять, чтобы достигать все
+                                поставленные цели.</p>
+                            <button>Получить доступ за 500,00 $</button>
+                        </div>
+                        <div className="effect-title2">
+                            <h2>В курс входит</h2>
+                            <p>43 материала</p>
+                            <p>100% эксклюзив</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <iframe width="1000" height="536" src="https://www.youtube.com/embed/V0qAq6ZLpic"
                     title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen style={{ marginLeft:"130px"}}></iframe>
+                    allowFullScreen style={{marginLeft: "130px"}}></iframe>
         </div>
+
     );
 };
 
