5e5f955b728a3a75f1ac897ab42a89b5036aa8c4
Nazi <126472056+Nazidev01@users.noreply.github.com>
2023-06-17 14:04:39 +0600

    savia (#32)

--- src/Pages/Paket/paket.js
+++ src/Pages/Paket/paket.js
@@ -1,24 +1,31 @@
-import React from "react"
+import React, {useContext} from "react"
 import book from "./img/tea.png"
 import "./paket.css"
 import {Link} from "react-router-dom";
+import {LanguageContext} from "../../components/Context";
 
 const Paket = () => {
+    const { language } = useContext(LanguageContext);
+    const { pack } = useContext(LanguageContext);
     return (
         <div>
             <section id="paket">
                 <div className="container">
-                    <div className="paket">
-                        <div className="paket--text">
-                            <h2 style={{color: "white"}}>Наши курсы</h2>
-                            <p>Получайте новые знания, навыки и <br/>возможности для эффективной жизни..</p>
-                            <Link to={"/buttons"}>
-                                <button className="paket--text__btn">Подобрать пакет</button>
-                            </Link>
-                            <button className="paket--text__btn2">Войти</button>
-                        </div>
-                        <img className="paket--img" src={book} alt=""/>
-                    </div>
+                    {
+                        pack.map((el) => (
+                            <div className="paket">
+                                <div className="paket--text">
+                                    <h2 style={{color: "white"}}>{language ? el.pack.textKG : el.pack.text}</h2>
+                                    <p>{language ? el.pack.text1KG : el.pack.text1}</p>
+                                    <Link to={"/buttons"}>
+                                        <button className="paket--text__btn">{language ? el.pack.text3KG : el.pack.text3}</button>
+                                    </Link>
+                                    <button className="paket--text__btn2">{language ? el.pack.text4KG : el.pack.text4}</button>
+                                </div>
+                                <img className="paket--img" src={book} alt=""/>
+                            </div>
+                        ))
+                    }
                 </div>
             </section>
         </div>
